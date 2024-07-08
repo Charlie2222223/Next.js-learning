ea4137847b6fb151b9ec51a9c5303e1a54b42c21
Hiroto Ikegawa <koko.rara20@icloud.com>
2024-07-08 22:21:53 +0900

    ナビゲーションバーの作成

--- src/components/Landing/HeroBanner.jsx
+++ src/components/Landing/HeroBanner.jsx
@@ -1,114 +1,128 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-
-/**
- * ウェブページのヒーローバナーセクションとして、背景画像のスライドショーと検索バー、人気のサービスリンクを表示
- */
-function HeroBanner() {
-    const router = useRouter();
-    const [image, setImage] = useState(3); // useStateでimageという状態を定義し、初期値を3に設定。
-
-    useEffect(() => {
-        const interval = setInterval(() => setImage(image > 6 ? 1 : image + 1), 10000); // useEffectを使用して、10秒ごとにimage状態を更新するタイマーを設定します   
-        return () => clearInterval(interval);
-    }, [image]);
-
-    return (
-        <div className="h-[680px] relative bg-cover">   {/* バナーの高さを680ピクセルに設定し、背景画像をカバーするように配置 */}
-            <div className="absolute top-0 right-0 w-[110vw] h-full transition-opacity z-0">
-                {/*
-                    absoluteクラスを使用して、背景画像のスライドショーを実装。
-                    それぞれのImageコンポーネントは、異なる背景画像を表示し、fill属性を使って親要素を完全にカバー。
-                    classNameプロパティを使用して、現在表示されるべき画像にopacity-100を、その他の画像にopacity-0を適用し、スライドショーの切り替えを実現
-                */}
-                <Image
-                    alt="hero"
-                    src="/bg-hero1.webp"
-                    fill
-                    sizes="100vw"
-                    priority
-                    className={`${image === 1 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-                <Image
-                    alt="hero"
-                    src="/bg-hero2.webp"
-                    fill
-                    sizes="100vw"
-                    className={`${image === 2 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-                <Image
-                    alt="hero"
-                    src="/bg-hero3.webp"
-                    fill
-                    sizes="100vw"
-                    priority
-                    className={`${image === 3 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-                <Image
-                    alt="hero"
-                    src="/bg-hero4.webp"
-                    fill
-                    sizes="100vw"
-                    className={`${image === 4 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-                <Image
-                    alt="hero"
-                    src="/bg-hero5.webp"
-                    fill
-                    sizes="100vw"
-                    className={`${image === 5 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-                <Image
-                    alt="hero"
-                    src="/bg-hero6.webp"
-                    fill
-                    sizes="100vw"
-                    className={`${image === 6 ? "opacity-100" : "opacity-0"} transition-all duration-1000`}
-                />
-            </div>
-            <div className="z-1 relative w-[650px] flex justify-center flex-col h-full gap-5 ml-20">
-                <h1 className="text-5xl leading-snug text-white">
-                    Find the perfect &nbsp;
-                    <i>Freelance</i>
-                    <br />
-                    services for your business.
-                </h1>
-                {/*
-                    z-1 relative w-[650px] flex justify-center flex-col h-full gap-5 ml-20のクラスを持つdiv要素は、
-                    テキストと検索バーを含むセクションのスタイルを設定。
-                    h1タグでメインメッセージを表示し、text-white、text-5xl、leading-snugのクラスでスタイルを設定。
-                */}
-                <div className="flex items-center">
-                    <div className="relative">
-                        <input
-                            type="text"
-                            className="h-14 w-[450px] pl-10 rounded-md rounded-r-none"
-                            placeholder='Try "building mobile app"'
-                        />
-                    </div>
-                    <button className="bg-[#1DBF73] text-white px-12 text-lg font-semibold rounded-r-md">Search</button>
-                </div>
-                <div className="flex gap-4 text-white">
-                    Popular:{" "}
-                    <ul className="flex gap-5">
-                        <li className="px-3 py-1 text-sm transition border rounded-full hover:bg-white hover:text-black">
-                            Website Design
-                        </li>
-                        <li className="px-3 py-1 text-sm transition border rounded-full hover:bg-white hover:text-black">
-                            Wordpress
-                        </li>
-                        <li className="px-3 py-1 text-sm transition border rounded-full hover:bg-white hover:text-black">
-                            Logo Design
-                        </li>
-                        <li className="px-3 py-1 text-sm transition border rounded-full hover:bg-white hover:text-black">
-                            AI Services
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+import { IoSearchOutline } from "react-icons/io5";
+function HomeBanner() {
+  const router = useRouter();
+  const [image, setImage] = useState(1);
+  const [searchData, setSearchData] = useState("");
+  useEffect(() => {
+    const interval = setInterval(
+      () => setImage(image >= 6 ? 1 : image + 1),
+      10000
     );
+    return () => clearInterval(interval);
+  }, [image]);
+
+  return (
+    <div className="h-[680px] relative bg-cover">
+      <div className="absolute top-0 right-0 w-[110vw] h-full transition-opacity z-0">
+        <Image
+          alt="hero"
+          src="/bg-hero1.webp"
+          fill
+          className={`${
+            image === 1 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero"
+          src="/bg-hero2.webp"
+          fill
+          className={`${
+            image === 2 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero"
+          src="/bg-hero3.webp"
+          fill
+          className={`${
+            image === 3 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero"
+          src="/bg-hero4.webp"
+          fill
+          className={`${
+            image === 4 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero"
+          src="/bg-hero5.webp"
+          fill
+          className={`${
+            image === 5 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+        <Image
+          alt="hero"
+          src="/bg-hero6.webp"
+          fill
+          className={`${
+            image === 6 ? "opacity-100" : "opacity-0"
+          } transition-all duration-1000`}
+        />
+      </div>
+      <div className="z-10 relative w-[650px] flex justify-center flex-col h-full gap-5 ml-20">
+        <h1 className="text-5xl leading-snug text-white">
+          Find the perfect&nbsp;
+          <i>freelance</i>
+          <br />
+          services for your business
+        </h1>
+        <div className="flex align-middle">
+          <div className="relative">
+            <IoSearchOutline className="absolute flex h-full text-2xl text-gray-500 align-middle left-2" />
+            <input
+              type="text"
+              className="h-14 w-[450px] pl-10 rounded-md rounded-r-none"
+              placeholder={`Try "building mobile app"`}
+              value={searchData}
+              onChange={(e) => setSearchData(e.target.value)}
+            />
+          </div>
+          <button
+            className="bg-[#1DBF73] text-white px-12 text-lg font-semibold rounded-r-md"
+            onClick={() => router.push(`/search?q=${searchData}`)}
+          >
+            Search
+          </button>
+        </div>
+        <div className="flex gap-4 text-white">
+          Popular:
+          <ul className="flex gap-5">
+            <li
+              className="px-3 py-1 text-sm transition-all duration-300 border rounded-full cursor-pointer hover:bg-white hover:text-black"
+              onClick={() => router.push("/search?q=website design")}
+            >
+              Website Design
+            </li>
+            <li
+              className="px-3 py-1 text-sm transition-all duration-300 border rounded-full cursor-pointer hover:bg-white hover:text-black"
+              onClick={() => router.push("/search?q=wordpress")}
+            >
+              Wordpress
+            </li>
+            <li
+              className="px-3 py-1 text-sm transition-all duration-300 border rounded-full cursor-pointer hover:bg-white hover:text-black"
+              onClick={() => router.push("/search?q=logo design")}
+            >
+              Logo Design
+            </li>
+            <li
+              className="px-3 py-1 text-sm transition-all duration-300 border rounded-full cursor-pointer hover:bg-white hover:text-black"
+              onClick={() => router.push("/search?q=ai services")}
+            >
+              AI Services
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HeroBanner;
+export default HomeBanner;
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,9 @@
 import Footer from "@/components/Footer";
+import Navber from "@/components/Navber";
 import { StateProvider } from "@/context/StateContext";
 import reducer, { initialState } from "@/context/StateReducers";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 /**
  * アプリケーション全体のレイアウトや共通の機能を設定するために使用
@@ -11,11 +13,18 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <StateProvider initialState={initialState} reducer={reducer}>
-      <div className="w-full mx-auto mb-auto">
-        <Component {...pageProps} />
+      <Head>
+        <link ref="shortcut icon" href="/favicon.ico" />
+        <title>Fiverr Clone</title>
+      </Head>
+      <div className="relative flex flex-col justify-between h-screen">  
+        <Navber />
+        <div className="w-full mx-auto mb-auto">
+          <Component {...pageProps} />
+        </div>
+        
+        <Footer />
       </div>
-      
-      <Footer />
     </StateProvider>
   );
 }
